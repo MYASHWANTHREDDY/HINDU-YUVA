2f573cb4d60d60e309b747d6d5551f8222fc5dcd
MYASHWANTHREDDY <yashwanthreddynov1@gmail.com>
2025-10-14 09:59:33 -0700

    news page updated

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -31,7 +31,7 @@ function News() {
   return (
   <div className="pt-[200px] p-10 min-h-screen bg-orange-50">
       <h1 className="text-4xl font-bold text-orange-600 mb-10 text-center">News & Announcements</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 px-8 md:px-20">
+      <div className="grid grid-cols-1 md:grid-cols-3  mx-auto px-8 justify-items-center">
         {news.map((item, idx) => (
           <div
             key={idx}
